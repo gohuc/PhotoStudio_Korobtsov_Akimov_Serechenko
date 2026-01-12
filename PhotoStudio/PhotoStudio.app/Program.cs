06577f59a4c0acd59bf6322bd32a8c6d8813e9c9
gohuc <164240966+gohuc@users.noreply.github.com>
2026-01-12 17:44:38 +0300

    init: console app skeleton

---
+++ PhotoStudio/PhotoStudio.app/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("TaskTracker v0.1");
+Console.WriteLine("----------------");
+Console.WriteLine("1) Добавить задачу (пока заглушка)");
+Console.WriteLine("2) Показать список задач (пока заглушка)");
+Console.WriteLine("3) Изменить статус задачи (пока заглушка)");
+Console.WriteLine("4) Удалить задачу (пока заглушка)");
+Console.WriteLine("0) Выход");
+Console.WriteLine("----------------");
+Console.Write("Выберите пункт меню: ");
+
+var input = Console.ReadLine();
+Console.WriteLine($"Вы выбрали: {input}");
+Console.WriteLine("Пока это только каркас. Логика появится в следующих ЛР.");
